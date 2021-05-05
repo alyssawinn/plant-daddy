8cbc8575bda7c9ce7e5269e8a4f3fd8b79eb30f7
Valdeth <alstontravis6@gmail.com>
2021-05-04 21:13:48 -0600

    plant type modle and some basic assosiations (commented out)

--- models/index.js
+++ models/index.js
@@ -1,6 +1,42 @@
 const User = require('./User');
-const Plant = require('./Plant');
+const PlantType = require('./PlantType');
 const PlantCategory = require('./PlantCategory');
 
+// User.hasMany(Plant, {
+//     foreignKey: 'user_id'
+// });
 
-module.exports = { User, Plant, PlantCategory,};
+// Plant.belongsTo(User, {
+//     foreignKey: 'user_id',
+//     onDelete: 'SET NULL'
+// });
+
+// User.belongsToMany(Plant, {
+//     foreignKey: 'user_id',
+//     onDelete: 'SET NULL'
+// });
+
+// Plant.belongsToMany(User, {
+//     foreignKey: 'plant_id',
+//     onDelete: 'SET NULL'
+// });
+
+// PlantCategory.belongsTo(User, {
+//     foreignKey: 'user_id',
+//     onDelete: 'SET NULL'
+// });
+
+// PlantCategory.belongsTo(Post, {
+//     foreignKey: 'plant_id',
+//     onDelete: 'SET NULL'
+// });
+
+// User.hasMany(PlantCategory, {
+//     foreignKey: 'user_id'
+// });
+
+// Plant.hasMany(PlantCategory, {
+//     foreignKey: 'plant_id'
+// });
+
+module.exports = { User, PlantType, PlantCategory,};
