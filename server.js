cf0b61e1f8cf784370e0146ea41e88b4c3f6abbf
Alyssa Winn <alyssawinn2@hotmail.com>
2021-05-04 19:29:03 -0600

    home-routes

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const sess = {
 
 //app.user(session(sess));
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
