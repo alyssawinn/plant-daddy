22c26f3a1fb129dfa996c3789240b326ba1ca3ec
Valdeth <alstontravis6@gmail.com>
2021-05-04 20:12:43 -0600

    home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,72 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User, Plant, PlantCategory } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  console.log('======================');
+  Plant.findAll({
+    attributes: [
+      'id',
+      'name',
+      'waterAmount',
+      'sunlightAmount',
+      'category_id'
+    ],
+  })
+    .then(dbPlantData => {
+      const plants = dbPlantData.map(plant => plant.get({ plain: true }));
+
+      res.render('homepage', {
+        plants,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// get single post
+router.get('/post/:id', (req, res) => {
+  Plant.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'name',
+      'waterAmount',
+      'sunlightAmount',
+      'category_id'
+    ],
+  })
+    .then(dbPlantData => {
+      if (!dbPlantData) {
+        res.status(404).json({ message: 'No plants found with this id' });
+        return;
+      }
+
+      const plant = dbPlantData.get({ plain: true });
+
+      res.render('single-post', {
+        plant,
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
