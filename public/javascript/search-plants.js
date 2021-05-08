ae9372b4c9f610ef9a702b95a57888915e628bf2
Alyssa Winn <alyssawinn2@hotmail.com>
2021-05-08 01:23:31 -0600

    addt save

--- public/javascript/search-plants.js
+++ public/javascript/search-plants.js
@@ -8,35 +8,33 @@ async function searchFormHandler(event) {
         .split(' ')
         .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
         .join(' ');
-    
+
 
     if (!(selectedCategory === "") || !(selectedName === "")) {
         const response = await fetch(`/api/plantType`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        }
-    }).then(response => response.json())
-    .then(data => {
-        for (i=0; i < data.length; i++) {
-            if ((selectedCategory == data[i].category_id)) {
-                selectedArray.push(data[i]);
-            } else if ((data[i].name.indexOf(selectedName) == 0) && (selectedName.length > 0)) {
-                selectedArray.push(data[i]);
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
             }
-        }
-        console.log(selectedArray);
-        if (selectedArray.length === 0) {
-            alert('No results. Please try again.')
-            document.location.reload();
-        }
-    })
-
-    
+        }).then(response => response.json())
+            .then(data => {
+                for (i = 0; i < data.length; i++) {
+                    if ((selectedCategory == data[i].category_id)) {
+                        selectedArray.push(data[i]);
+                    } else if ((data[i].name.indexOf(selectedName) == 0) && (selectedName.length > 0)) {
+                        selectedArray.push(data[i]);
+                    }
+                }
+                console.log(selectedArray);
+                if (selectedArray.length === 0) {
+                    alert('No results. Please try again.')
+                    document.location.reload();
+                }
+            })
     }
 
-    
+
 }
 
 document.querySelector('.search-bar-form').addEventListener('submit', searchFormHandler);
